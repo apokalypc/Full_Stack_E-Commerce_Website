eec25b30da0df286c6cac31c6305c0d805dc6177
Trevor Ewart <tmwai@kabarak.ac.ke>
2026-01-31 17:44:28 +0300

    Added a cart page and designed it, Implemented adding selected products to he cart page along with corresponding product details including price, size and quantity, Implemented dynamically adjusting value at cart iconadded a related products section

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,15 +1,16 @@
-import React, { use, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { useState } from 'react'
 import { assets } from '../assets/assets'
+import RelatedProducts from '../components/RelatedProducts'
 
 
 const Product = () => {
 
   const { productId } = useParams();
-  const { products, currency } = useContext(ShopContext);
+  const { products, currency, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('');
   const [size,setSize] = useState('');
@@ -32,7 +33,7 @@ const Product = () => {
   }, [productId, products]);
 
   return productData ? (
-    <div className='borer-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
       {/* Product Data */}
       <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
 
@@ -76,7 +77,7 @@ const Product = () => {
                 </div>
 
               </div>
-              <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+              <button onClick={() => addToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
               <hr className='mt-8 sm:w-4/5'/>
               <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
                 <p>100% Original Product </p>
@@ -87,6 +88,25 @@ const Product = () => {
         </div>
 
       </div>
+
+      {/* Description and review section */}
+
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p>Welcome to our e-commerce store. Shop quality products at great prices. Fast delivery, easy payments, and secure shopping, all in one place</p>
+          <p>Discover the best deals on fashion, electronics, and more. Simple shopping, trusted products, and quick doorstep delivery every time</p>
+
+        </div>
+
+      </div>
+
+      {/* Display related products */}
+
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
 
     </div>
   ) : <div className='opacity-0'></div>
