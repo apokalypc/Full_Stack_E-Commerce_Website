f7a8510f18965bc144db70582edca23f11d88357
Trevor Ewart <tmwai@kabarak.ac.ke>
2026-02-14 14:43:44 +0300

    feat: add singleProduct controller to fetch product by ID, implement adminLogin with JWT authentication, create adminAuth middleware for token verification, secure product routes with adminAuth and multer upload, add admin folder and set up dependencies

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -86,7 +86,16 @@ const removeProduct = async (req, res) => {
 // Functions for single product info
 const singleProduct = async (req, res) => {
 
+    try {
+        
+        const {productId} = req.body
+        const product = await productModel.findById(productId)
+        res.json({success:true,product})
 
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 
 }
 
