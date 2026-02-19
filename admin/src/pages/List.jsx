d2c525c60a53890f6d20ccaf31f124a7079b3a39
Trevor Ewart <tmwai@kabarak.ac.ke>
2026-02-19 13:48:35 +0300

    Add admin panel UI with login, navbar, sidebar, and product upload functionality

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { backendUrl } from '../App'
+import { toast } from 'react-toastify'
 
 const List = () => {
-  return (
-    <div>
+
+  const [list,setList] = useState([])
+
+  const fetchList = async () => {
+    try {
       
-    </div>
+      const response = await axios.get(backendUrl + '/api/product/list')
+      if (response.data.success) {
+        setList(response.data.products);
+      }
+      else {
+        toast.error(response.data.message)
+      }
+          
+
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+      
+    }
+  }
+
+  useEffect(()=>{
+    fetchList()
+  },[])
+
+  return (
+    <>
+      <p className='mb-2'>All Products List</p>
+      <div>
+
+        {/* List Table Title */}
+
+      </div>
+    </>
   )
 }
 
