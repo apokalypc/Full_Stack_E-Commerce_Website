193437e2c02bb2fa32bc739d15f02793263af680
Trevor Ewart <tmwai@kabarak.ac.ke>
2026-03-24 15:00:52 +0300

    Added screenshots folder for readme section in github, removed uploads folder

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -9,11 +9,11 @@ const NewsletterBox = () => {
   return (
     <div className='text-center'>
       <p className='text-2xl font-medium text-gray-800' >Suscribe now & get 20% off on your first order.</p>
-      <p className='text-gry-400 mt-3'>
+      <p className='text-gray-400 mt-3'>
         Join our newsletter to stay updated with the latest news and exclusive offers.
       </p>
       <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
-        <input className='-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' required />
+        <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' required />
         <button type='submit' className='bg-black text-white text-xs px-10 py-4'>SUBSCRIBE</button>
       </form>
     </div>
--- frontend/src/components/OurPolicy.jsx
+++ frontend/src/components/OurPolicy.jsx
@@ -7,17 +7,17 @@ const OurPolicy = () => {
 
        <div>
         <img src={assets.exchange_icon} className='w-12 m-auto mb-5' alt="" />
-        <p className='font-seibold'>Easy Exchange Policy</p>
+        <p className='font-semibold'>Easy Exchange Policy</p>
         <p className='text-gray-400'>We offer a hassle-free exchange policy for all our products.</p>
        </div>
        <div>
         <img src={assets.quality_icon} className='w-12 m-auto mb-5' alt="" />
-        <p className='font-seibold'>7 Days Return Policy</p>
+        <p className='font-semibold'>7 Days Return Policy</p>
         <p className='text-gray-400'>We provide a 7-day free return policy for all our products.</p>
        </div>
        <div>
         <img src={assets.support_img} className='w-12 m-auto mb-5' alt="" />
-        <p className='font-seibold'>Best Customer Support</p>
+        <p className='font-semibold'>Best Customer Support</p>
         <p className='text-gray-400'>We offer 24/7 customer support.</p>
        </div>
     </div>
